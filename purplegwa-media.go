9fcb614cafc3bd65262c213e0b4436bbfcc42d18
Hermann Höhne <hoehermann@gmx.de>
2019-07-07 13:18:06 +0200

    Use gofmt.

--- purplegwa-media.go
+++ purplegwa-media.go
@@ -1,107 +1,107 @@
 package main
 
 import (
-    "C"
-    "fmt"
+	"C"
+	"fmt"
 	"os"
 	"path/filepath"
-    "strings"
-    
-    "github.com/Rhymen/go-whatsapp"
+	"strings"
+
+	"github.com/Rhymen/go-whatsapp"
 )
 
 func (handler *waHandler) sendMediaMessage(info whatsapp.MessageInfo, text string) *C.char {
-    data, err := os.Open(filepath.Join(handler.downloadsDirectory, "outgoing"))
-    if err != nil {
-        handler.messages <- makeConversationErrorMessage(info,
-            fmt.Sprintf("Unable to read file which was going to be sent: %v", err))
-            return nil
-    }
-    // TODO: guess mime type
-    if (strings.Contains(text, "image")) {
-        message := whatsapp.ImageMessage{
-            Info: info,
-            Type: "image/jpeg",
-            Content: data,
-        }
-        return handler.sendMessage(message, info)
-    } else if (strings.Contains(text, "audio")) {
-        message := whatsapp.AudioMessage{
-            Info: info,
-            Type: "audio/ogg",
-            Content: data,
-        }
-        return handler.sendMessage(message, info)
-    } else {
-        handler.messages <- makeConversationErrorMessage(info,
-            "Please specify file type image or audio")
-        return nil
-    }
+	data, err := os.Open(filepath.Join(handler.downloadsDirectory, "outgoing"))
+	if err != nil {
+		handler.messages <- makeConversationErrorMessage(info,
+			fmt.Sprintf("Unable to read file which was going to be sent: %v", err))
+		return nil
+	}
+	// TODO: guess mime type
+	if strings.Contains(text, "image") {
+		message := whatsapp.ImageMessage{
+			Info:    info,
+			Type:    "image/jpeg",
+			Content: data,
+		}
+		return handler.sendMessage(message, info)
+	} else if strings.Contains(text, "audio") {
+		message := whatsapp.AudioMessage{
+			Info:    info,
+			Type:    "audio/ogg",
+			Content: data,
+		}
+		return handler.sendMessage(message, info)
+	} else {
+		handler.messages <- makeConversationErrorMessage(info,
+			"Please specify file type image or audio")
+		return nil
+	}
 }
 
 func isSaneId(s string) bool {
-    for _, r := range s {
-        if (r < 'A' || r > 'Z') && (r < '0' || r > '9') {
-            return false
-        }
-    }
-    return true
+	for _, r := range s {
+		if (r < 'A' || r > 'Z') && (r < '0' || r > '9') {
+			return false
+		}
+	}
+	return true
 }
 
 func generateFilepath(downloadsDirectory string, info whatsapp.MessageInfo) string {
-    fp, _ := filepath.Abs(filepath.Join(downloadsDirectory, info.Id))
-    return fp
+	fp, _ := filepath.Abs(filepath.Join(downloadsDirectory, info.Id))
+	return fp
 }
 
 func (handler *waHandler) wantToDownload(info whatsapp.MessageInfo) (filename string, want bool) {
-    fp := generateFilepath(handler.downloadsDirectory, info)
-    fileInfo, err := os.Stat(fp)
-    return fp, os.IsNotExist(err) || fileInfo.Size() == 0
+	fp := generateFilepath(handler.downloadsDirectory, info)
+	fileInfo, err := os.Stat(fp)
+	return fp, os.IsNotExist(err) || fileInfo.Size() == 0
 }
 
 func (handler *waHandler) storeDownloadedData(info whatsapp.MessageInfo, filename string, data []byte) {
-    os.MkdirAll(handler.downloadsDirectory, os.ModePerm)
-    file, err := os.Create(filename)
-    defer file.Close()
-    if err != nil {
-        handler.messages <- makeConversationErrorMessage(info,
-            fmt.Sprintf("Data was downloaded, but file %s creation failed due to %v", filename, err))
-    } else {
-        _, err := file.Write(data)
-        if err != nil {
-        handler.messages <- makeConversationErrorMessage(info,
-            fmt.Sprintf("Data was downloaded, but could not be written to file %s due to %v", filename, err))
-        } else {
-            handler.messages <- MessageAggregate{
-                text : fmt.Sprintf("file://%s", filename),
-                info : info,
-                system : true}
-        }
-    }
+	os.MkdirAll(handler.downloadsDirectory, os.ModePerm)
+	file, err := os.Create(filename)
+	defer file.Close()
+	if err != nil {
+		handler.messages <- makeConversationErrorMessage(info,
+			fmt.Sprintf("Data was downloaded, but file %s creation failed due to %v", filename, err))
+	} else {
+		_, err := file.Write(data)
+		if err != nil {
+			handler.messages <- makeConversationErrorMessage(info,
+				fmt.Sprintf("Data was downloaded, but could not be written to file %s due to %v", filename, err))
+		} else {
+			handler.messages <- MessageAggregate{
+				text:   fmt.Sprintf("file://%s", filename),
+				info:   info,
+				system: true}
+		}
+	}
 }
 
 type downloadable interface {
-    Download() ([]byte, error)
+	Download() ([]byte, error)
 }
 
-func (handler *waHandler) downloadMessage (message downloadable, info whatsapp.MessageInfo) {
-    filename, wtd := handler.wantToDownload(info)
-    if (wtd) {
-        if (handler.doDownloads) {
-            if isSaneId(info.Id) {
-                data, err := message.Download()
-                if err != nil {
-                    handler.messages <- makeConversationErrorMessage(info,
-                        fmt.Sprintf("A media message (ID %s) was received, but the download failed: %v", info.Id, err))
-                } else {
-                    handler.storeDownloadedData(info, filename, data)
-                }
-            } else {
-                handler.messages <- makeConversationErrorMessage(info,
-                    fmt.Sprintf("A media message (ID %s) was received, but ID looks odd – downloading skipped.", info.Id))
-            }
-        } else {
-            handler.messages <- MessageAggregate{text : "[File download disabled in settings.]", system : true}
-        }
-    }
+func (handler *waHandler) downloadMessage(message downloadable, info whatsapp.MessageInfo) {
+	filename, wtd := handler.wantToDownload(info)
+	if wtd {
+		if handler.doDownloads {
+			if isSaneId(info.Id) {
+				data, err := message.Download()
+				if err != nil {
+					handler.messages <- makeConversationErrorMessage(info,
+						fmt.Sprintf("A media message (ID %s) was received, but the download failed: %v", info.Id, err))
+				} else {
+					handler.storeDownloadedData(info, filename, data)
+				}
+			} else {
+				handler.messages <- makeConversationErrorMessage(info,
+					fmt.Sprintf("A media message (ID %s) was received, but ID looks odd – downloading skipped.", info.Id))
+			}
+		} else {
+			handler.messages <- MessageAggregate{text: "[File download disabled in settings.]", system: true}
+		}
+	}
 }
--- purplegwa.go
+++ purplegwa.go
@@ -38,30 +38,31 @@ struct gowhatsapp_session {
 import "C"
 
 import (
-    "encoding/base64"
+	"encoding/base64"
 	"fmt"
 	"os"
-	"time"
 	"strings"
+	"time"
 
 	"github.com/Rhymen/go-whatsapp"
 	"github.com/skip2/go-qrcode"
 )
 
 type MessageAggregate struct {
-    info whatsapp.MessageInfo
-    text string
+	info    whatsapp.MessageInfo
+	text    string
 	session *whatsapp.Session
-	data []byte
-	err error
-	system bool
+	data    []byte
+	err     error
+	system  bool
 }
 type waHandler struct {
-	wac *whatsapp.Conn
-	messages chan MessageAggregate
+	wac                *whatsapp.Conn
+	messages           chan MessageAggregate
 	downloadsDirectory string
-	doDownloads bool
+	doDownloads        bool
 }
+
 var waHandlers = make(map[C.uintptr_t]*waHandler)
 
 func (handler *waHandler) sendMessage(message interface{}, info whatsapp.MessageInfo) *C.char {
@@ -69,7 +70,7 @@ func (handler *waHandler) sendMessage(message interface{}, info whatsapp.Message
 	if err != nil {
 		handler.messages <- makeConversationErrorMessage(info,
 			fmt.Sprintf("Unable to send message: %v", err))
-			return nil
+		return nil
 	}
 	return C.CString(msgId)
 }
@@ -85,7 +86,7 @@ func gowhatsapp_go_sendMessage(connID C.uintptr_t, who *C.char, text *C.char) *C
 		RemoteJid: remoteJid,
 	}
 	gotext := C.GoString(text)
-	if (strings.HasPrefix(gotext, "/sendmedia")) {
+	if strings.HasPrefix(gotext, "/sendmedia") {
 		return handler.sendMediaMessage(info, gotext)
 	} else {
 		message := whatsapp.TextMessage{
@@ -98,7 +99,7 @@ func gowhatsapp_go_sendMessage(connID C.uintptr_t, who *C.char, text *C.char) *C
 
 // TODO: find out how to enable C99's bool type in cgo
 func bool_to_Cchar(b bool) C.char {
-	if (b) {
+	if b {
 		return C.char(1)
 	} else {
 		return C.char(0)
@@ -108,47 +109,47 @@ func bool_to_Cchar(b bool) C.char {
 //export gowhatsapp_go_getMessage
 func gowhatsapp_go_getMessage(connID C.uintptr_t) C.struct_gowhatsapp_message {
 	handler, ok := waHandlers[connID]
-	if (!ok) {
+	if !ok {
 		return C.struct_gowhatsapp_message{
-			msgtype : C.int64_t(C.gowhatsapp_message_type_error),
-			text    : C.CString("Tried to get a message from a non-existant connection. Please file a bug.")}
+			msgtype: C.int64_t(C.gowhatsapp_message_type_error),
+			text:    C.CString("Tried to get a message from a non-existant connection. Please file a bug.")}
 	}
 	select {
-	case message := <- handler.messages:
+	case message := <-handler.messages:
 		//fmt.Printf("%+v\n", message)
-		if (message.err != nil) {
+		if message.err != nil {
 			// thanks to https://stackoverflow.com/questions/39023475/
 			return C.struct_gowhatsapp_message{
-				msgtype : C.int64_t(C.gowhatsapp_message_type_error),
-				text    : C.CString(message.err.Error()),
+				msgtype: C.int64_t(C.gowhatsapp_message_type_error),
+				text:    C.CString(message.err.Error()),
 			}
 		}
-		if (message.session != nil) {
+		if message.session != nil {
 			return C.struct_gowhatsapp_message{
-			    msgtype     : C.int64_t(C.gowhatsapp_message_type_session),
-				clientId    : C.CString(message.session.ClientId),
-				clientToken : C.CString(message.session.ClientToken),
-				serverToken : C.CString(message.session.ServerToken),
-				encKey_b64  : C.CString(base64.StdEncoding.EncodeToString(message.session.EncKey)),
-				macKey_b64  : C.CString(base64.StdEncoding.EncodeToString(message.session.MacKey)),
-				wid         : C.CString(message.session.Wid),
+				msgtype:     C.int64_t(C.gowhatsapp_message_type_session),
+				clientId:    C.CString(message.session.ClientId),
+				clientToken: C.CString(message.session.ClientToken),
+				serverToken: C.CString(message.session.ServerToken),
+				encKey_b64:  C.CString(base64.StdEncoding.EncodeToString(message.session.EncKey)),
+				macKey_b64:  C.CString(base64.StdEncoding.EncodeToString(message.session.MacKey)),
+				wid:         C.CString(message.session.Wid),
 			}
 		}
 		info := message.info
-		if (!message.system) {
-		    handler.wac.Read(info.RemoteJid, info.Id) // mark message as "displayed"
+		if !message.system {
+			handler.wac.Read(info.RemoteJid, info.Id) // mark message as "displayed"
 		} else {
-		    info.Id = ""
+			info.Id = ""
 		}
 		return C.struct_gowhatsapp_message{
-		    msgtype   : C.int64_t(C.gowhatsapp_message_type_text),
-			timestamp : C.time_t(info.Timestamp),
-			id        : C.CString(info.Id),
-			remoteJid : C.CString(info.RemoteJid),
-			senderJid : C.CString(info.SenderJid),
-			fromMe    : bool_to_Cchar(info.FromMe),
-			text      : C.CString(message.text),
-			system    : bool_to_Cchar(message.system),
+			msgtype:   C.int64_t(C.gowhatsapp_message_type_text),
+			timestamp: C.time_t(info.Timestamp),
+			id:        C.CString(info.Id),
+			remoteJid: C.CString(info.RemoteJid),
+			senderJid: C.CString(info.SenderJid),
+			fromMe:    bool_to_Cchar(info.FromMe),
+			text:      C.CString(message.text),
+			system:    bool_to_Cchar(message.system),
 		}
 	default:
 		return C.struct_gowhatsapp_message{}
@@ -157,57 +158,57 @@ func gowhatsapp_go_getMessage(connID C.uintptr_t) C.struct_gowhatsapp_message {
 }
 
 func (handler *waHandler) HandleError(err error) {
-	if (strings.Contains(err.Error(), whatsapp.ErrInvalidWsData.Error())) { // TODO: less ugly error comparison
-	    //fmt.Fprintf(os.Stderr, "gowhatsapp: %v ignored.\n", err)
+	if strings.Contains(err.Error(), whatsapp.ErrInvalidWsData.Error()) { // TODO: less ugly error comparison
+		//fmt.Fprintf(os.Stderr, "gowhatsapp: %v ignored.\n", err)
 	} else {
 		fmt.Fprintf(os.Stderr, "gowhatsapp: error occoured: %v", err)
-		handler.messages <- MessageAggregate{err : err}
+		handler.messages <- MessageAggregate{err: err}
 	}
 }
 
 func makeConversationErrorMessage(originalInfo whatsapp.MessageInfo, errorMessage string) MessageAggregate {
-    return MessageAggregate{system: true, info : originalInfo, text : errorMessage}
+	return MessageAggregate{system: true, info: originalInfo, text: errorMessage}
 }
 
 func (handler *waHandler) HandleTextMessage(message whatsapp.TextMessage) {
-    handler.messages <- MessageAggregate{text : message.Text, info : message.Info}
+	handler.messages <- MessageAggregate{text: message.Text, info: message.Info}
 }
 
 func (handler *waHandler) HandleImageMessage(message whatsapp.ImageMessage) {
-    handler.messages <- MessageAggregate{text : message.Caption, info : message.Info}
-    handler.downloadMessage(&message, message.Info)
+	handler.messages <- MessageAggregate{text: message.Caption, info: message.Info}
+	handler.downloadMessage(&message, message.Info)
 }
 
 func (handler *waHandler) HandleVideoMessage(message whatsapp.VideoMessage) {
-    handler.messages <- MessageAggregate{text : message.Caption, info : message.Info}
-    handler.downloadMessage(&message, message.Info)
+	handler.messages <- MessageAggregate{text: message.Caption, info: message.Info}
+	handler.downloadMessage(&message, message.Info)
 }
 
 func (handler *waHandler) HandleAudioMessage(message whatsapp.AudioMessage) {
-    handler.downloadMessage(&message, message.Info)
+	handler.downloadMessage(&message, message.Info)
 }
 
 func (handler *waHandler) HandleDocumentMessage(message whatsapp.DocumentMessage) {
-    handler.downloadMessage(&message, message.Info)
+	handler.downloadMessage(&message, message.Info)
 }
 
 func connect_and_login(handler *waHandler, session *whatsapp.Session) {
 	//create new WhatsApp connection
 	wac, err := whatsapp.NewConn(
 		10 * time.Second, // TODO: make timeout user configurable
-		//"github.com/kaxap/go-whatsapp", "Pidgin via go-whatsapp"
-		)
+	//"github.com/kaxap/go-whatsapp", "Pidgin via go-whatsapp"
+	)
 	handler.wac = wac
 	if err != nil {
 		wac = nil
-		handler.messages <- MessageAggregate{err : err}
+		handler.messages <- MessageAggregate{err: err}
 		fmt.Fprintf(os.Stderr, "gowhatsapp error creating connection: %v\n", err)
 	} else {
-	    wac.AddHandler(handler)
+		wac.AddHandler(handler)
 		err = login(handler, session)
 		if err != nil {
 			wac = nil
-			handler.messages <- MessageAggregate{err : err}
+			handler.messages <- MessageAggregate{err: err}
 			fmt.Fprintf(os.Stderr, "gowhatsapp error logging in: %v\n", err)
 		}
 	}
@@ -224,27 +225,27 @@ func gowhatsapp_go_login(
 	wid *C.char,
 	downloadsDirectory *C.char,
 	doDownloads bool,
-	) {
+) {
 	// TODO: protect against concurrent invocation
-	handler := waHandler {
-		wac : nil,
-		messages : make(chan MessageAggregate, 100),
-		downloadsDirectory : C.GoString(downloadsDirectory),
-		doDownloads : doDownloads,
+	handler := waHandler{
+		wac:                nil,
+		messages:           make(chan MessageAggregate, 100),
+		downloadsDirectory: C.GoString(downloadsDirectory),
+		doDownloads:        doDownloads,
 	}
 	waHandlers[connID] = &handler
-	var session *whatsapp.Session;
-	if (clientId != nil && clientToken != nil && serverToken != nil && encKey_b64 != nil && macKey_b64 != nil && wid != nil) {
+	var session *whatsapp.Session
+	if clientId != nil && clientToken != nil && serverToken != nil && encKey_b64 != nil && macKey_b64 != nil && wid != nil {
 		encKey, encKeyErr := base64.StdEncoding.DecodeString(C.GoString(encKey_b64))
 		macKey, macKeyErr := base64.StdEncoding.DecodeString(C.GoString(macKey_b64))
-		if (encKeyErr == nil && macKeyErr == nil) {
+		if encKeyErr == nil && macKeyErr == nil {
 			session = &whatsapp.Session{
-				ClientId : C.GoString(clientId),
-				ClientToken : C.GoString(clientToken),
-				ServerToken : C.GoString(serverToken),
-				EncKey : encKey,
-				MacKey : macKey,
-				Wid : C.GoString(wid)}
+				ClientId:    C.GoString(clientId),
+				ClientToken: C.GoString(clientToken),
+				ServerToken: C.GoString(serverToken),
+				EncKey:      encKey,
+				MacKey:      macKey,
+				Wid:         C.GoString(wid)}
 		}
 	}
 	go connect_and_login(&handler, session)
@@ -254,8 +255,8 @@ func gowhatsapp_go_login(
 func gowhatsapp_go_close(connID C.uintptr_t) {
 	fmt.Fprintf(os.Stderr, "gowhatsapp close()\n")
 	handler, ok := waHandlers[connID]
-	if (ok) {
-		if (handler.wac != nil) {
+	if ok {
+		if handler.wac != nil {
 			handler.wac.Disconnect()
 		}
 		delete(waHandlers, connID)
@@ -263,7 +264,7 @@ func gowhatsapp_go_close(connID C.uintptr_t) {
 }
 
 func login(handler *waHandler, login_session *whatsapp.Session) error {
-    wac := handler.wac
+	wac := handler.wac
 	if login_session != nil {
 		//restore session
 		session, err := wac.RestoreWithSession(*login_session)
@@ -271,42 +272,42 @@ func login(handler *waHandler, login_session *whatsapp.Session) error {
 			return fmt.Errorf("restoring failed: %v\n", err)
 			// NOTE: "restore session connection timed out" may indicate phone switched off OR session data being invalid (after log-out via phone) O_o
 		}
-		handler.messages <- MessageAggregate{session : &session}
+		handler.messages <- MessageAggregate{session: &session}
 	} else {
 		//no saved session -> login via qr code
 		qr := make(chan string)
 		go func() {
 			png, err := qrcode.Encode(<-qr, qrcode.Medium, 256) // TODO: make size user configurable
 			if err != nil {
-			    handler.messages <- MessageAggregate{err : fmt.Errorf("login qr code generation failed: %v\n", err)}
+				handler.messages <- MessageAggregate{err: fmt.Errorf("login qr code generation failed: %v\n", err)}
 			} else {
-			    messageInfo := whatsapp.MessageInfo{
-				    RemoteJid: "login@s.whatsapp.net"}
+				messageInfo := whatsapp.MessageInfo{
+					RemoteJid: "login@s.whatsapp.net"}
 				handler.messages <- MessageAggregate{
-				    text : "Scan this QR code within 20 seconds to log in.",
-					info : messageInfo,
-					system : true}
+					text:   "Scan this QR code within 20 seconds to log in.",
+					info:   messageInfo,
+					system: true}
 				filename := generateFilepath(handler.downloadsDirectory, messageInfo)
 				handler.storeDownloadedData(messageInfo, filename, png)
 			}
 		}()
 		session, err := wac.Login(qr)
 		if err != nil {
-		    return fmt.Errorf("error during login: %v\n", err)
+			return fmt.Errorf("error during login: %v\n", err)
 		}
-		handler.messages <- MessageAggregate{session : &session}
+		handler.messages <- MessageAggregate{session: &session}
 		messageInfo := whatsapp.MessageInfo{
 			RemoteJid: "login@s.whatsapp.net"}
 		handler.messages <- MessageAggregate{
-		    text : "You are now logged in. You may close this window.",
-			info : messageInfo,
-			system : true}
+			text:   "You are now logged in. You may close this window.",
+			info:   messageInfo,
+			system: true}
 	}
 	return nil
 }
 
 func main() {
-    gowhatsapp_go_login(0, nil, nil, nil, nil, nil, nil, C.CString("."), true)
+	gowhatsapp_go_login(0, nil, nil, nil, nil, nil, nil, C.CString("."), true)
 	<-time.After(1 * time.Minute)
 	gowhatsapp_go_close(0)
 }
