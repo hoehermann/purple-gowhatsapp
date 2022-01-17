0804c4ad40921b29d423ce128c21cd22c279047f
Hermann Höhne <hoehermann@gmx.de>
2022-01-17 23:09:46 +0100

    Add spectrum compatibility option to list of options.

--- src/c/options.c
+++ src/c/options.c
@@ -22,6 +22,18 @@ gowhatsapp_add_account_options(GList *account_options)
         );
     account_options = g_list_append(account_options, option);
 
+    GList *choices = NULL;
+    choices = add_choice(choices, "Immediately", GOWHATSAPP_SEND_RECEIPT_CHOICE_IMMEDIATELY);
+    choices = add_choice(choices, "When interacting with conversation (buggy)", GOWHATSAPP_SEND_RECEIPT_CHOICE_ON_INTERACT);
+    choices = add_choice(choices, "When sending a reply", GOWHATSAPP_SEND_RECEIPT_CHOICE_ON_ANSWER);
+    choices = add_choice(choices, "Never", GOWHATSAPP_SEND_RECEIPT_CHOICE_NEVER);
+    option = purple_account_option_list_new(
+        "Send receipts",
+        GOWHATSAPP_SEND_RECEIPT_OPTION,
+        choices
+    );
+    account_options = g_list_append(account_options, option);
+    
     option = purple_account_option_bool_new(
         "Display offline contacts as away",
         GOWHATSAPP_FAKE_ONLINE_OPTION,
@@ -36,18 +48,6 @@ gowhatsapp_add_account_options(GList *account_options)
         );
     account_options = g_list_append(account_options, option);
     
-    GList *choices = NULL;
-    choices = add_choice(choices, "Immediately", GOWHATSAPP_SEND_RECEIPT_CHOICE_IMMEDIATELY);
-    choices = add_choice(choices, "When interacting with conversation (buggy)", GOWHATSAPP_SEND_RECEIPT_CHOICE_ON_INTERACT);
-    choices = add_choice(choices, "When sending a reply", GOWHATSAPP_SEND_RECEIPT_CHOICE_ON_ANSWER);
-    choices = add_choice(choices, "Never", GOWHATSAPP_SEND_RECEIPT_CHOICE_NEVER);
-    option = purple_account_option_list_new(
-        "Send receipts",
-        GOWHATSAPP_SEND_RECEIPT_OPTION,
-        choices
-    );
-    account_options = g_list_append(account_options, option);
-    
     option = purple_account_option_bool_new(
         "Download user profile pictures",
         GOWHATSAPP_GET_ICONS_OPTION,
@@ -55,5 +55,12 @@ gowhatsapp_add_account_options(GList *account_options)
         );
     account_options = g_list_append(account_options, option);
     
+    option = purple_account_option_bool_new(
+        "Treat system messages like normal messages (spectrum2 compatibility)",
+        GOWHATSAPP_SPECTRUM_COMPATIBILITY,
+        FALSE
+        );
+    account_options = g_list_append(account_options, option);
+
     return account_options;
 }
