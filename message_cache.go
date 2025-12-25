1f55baec41016f72aa44979ef84d68a53999f65f
Hermann Höhne <hoehermann@gmx.de>
2025-12-25 20:18:02 +0100

    Add notes in regard to building a poll vote message.

--- message_cache.go
+++ message_cache.go
@@ -55,6 +55,7 @@ func (handler *Handler) add_to_cache(message *waE2E.Message, id types.MessageID,
 		Chat:      chat,
 		Sender:    sender,
 		Timestamp: timestamp,
+		// also necessary for BuildPollVote: FromMe, IsGroup
 	})
 	// from https://www.delftstack.com/howto/go/queue-implementation-in-golang/
 	if len(handler.cachedMessages) > purple_get_int(handler.account, C.GOWHATSAPP_MESSAGE_CACHE_SIZE_OPTION, 0) {
